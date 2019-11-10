83665f0a4426ed62a7dbf8f6a9886a130b89c754
Sanjam Kumar Panda <36253777+sanjam008@users.noreply.github.com>
2019-11-10 18:57:33 +0000

    Update client.c

--- client.c
+++ client.c
@@ -12,7 +12,7 @@ main()
 	sid = socket(PF_INET,SOCK_STREAM,IPPROTO_TCP); //socket creation
 	q.sin_family=PF_INET;
 	q.sin_port = 2000;
-	q.sin_addr.s_addr=0;
+	q.sin_addr.s_addr=inet_addr("192.168.1.1");//IP of the server
 	status = connect(sid,&q,len); //connects to the server
 	if(status==-1) //if fails to connect to the server
 	{
