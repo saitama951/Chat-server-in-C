517b71b2e7df90fcff2b1d27f6f7e4caded59578
bad_boy17 <rvk8878@gmail.com>
2019-11-28 22:30:51 +0530

    updated server.c, client.c. prepared a makefile. updated readme file.

--- client.c
+++ client.c
@@ -1,33 +1,48 @@
 #include "netinet/in.h"
 #include "sys/socket.h"
 #include "string.h"
+#include "stdio.h"
+#include "arpa/inet.h"
 
 main()
 {
-	
-	int sid,status,n=1;
+
+	int sid, status, n = 1;
 	struct sockaddr_in q;
-	int len = sizeof(struct sockaddr_in);
+	int len = sizeof(q);
 	char x[100];
-	sid = socket(PF_INET,SOCK_STREAM,IPPROTO_TCP); //socket creation
-	q.sin_family=PF_INET;
-	q.sin_port = 2000;
-	q.sin_addr.s_addr=inet_addr("192.168.1.1");//IP of the server
-	status = connect(sid,&q,len); //connects to the server
-	if(status==-1) //if fails to connect to the server
+
+	//socket creation
+	sid = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	// setting up the socket
+	q.sin_family = PF_INET;
+	q.sin_port = 8080;
+
+	//IP of the server
+	q.sin_addr.s_addr = inet_addr("192.168.1.5");
+	status = connect(sid, (struct sockaddr *)&q, len);
+
+	//if fails to connect to the server
+	if(status == -1)
 	{
 		printf("connection failed");
 		exit(0);
 	}
-	while(1) 
+
+	// communication
+	while(1)
 	{
-		printf("enter data to send----->");
-		gets(x);
-		write(sid,x,strlen(x)); //send data to the server
-		
-		if(strcmp(x,"bye")==0) //disconnect from the server
+		printf("> ");
+		fgets(x, 99, stdin);
+
+		//send data to the server
+		send(sid, x, strlen(x), 0);
+
+		//disconnect from the server
+		if(strcmp(x, "bye\n") == 0)
 			break;
 	}
-		
-	
+
+
 }
